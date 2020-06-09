27af08febf75b12454bd4602310f1f4d6aa2dc71
leonardokatayama <leonardo@akata.com.br>
2020-06-09 14:46:46 -0300

    Alteração de teste

--- app/Controllers/Http/EstadoController.js
+++ app/Controllers/Http/EstadoController.js
@@ -1,37 +1,38 @@
-'use strict'
+"use strict";
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-const Estado = use('App/Models/Estado')
+const Estado = use("App/Models/Estado");
 
 /**
  * Resourceful controller for interacting with estados
  */
 class EstadoController {
+  async index() {
+    const estados = await Estado.query().with("pais").fetch();
+    // add varios comentarios
 
-  async index () {
-    const estados = await Estado.query().with('pais').fetch();
-
-    return estados 
+    return estados;
   }
 
-  async show ({ params }) {
-    const estado = await Estado.findOrFail(params.id)
-      await estado.load('pais')
-      //await estado.getRelated('pais')
-    return estado
-}
-
-  async store ({ request, response }){
-    const data = request.only(Estado.fillable);
-
-    const estado = await Estado.create(data);
-    
+  async show({ params }) {
+    const estado = await Estado.findOrFail(params.id);
+    await estado.load("pais");
+    // add varios comentarios
+    //await estado.getRelated('pais')
     return estado;
   }
 
+  async store({ request, response }) {
+    const data = request.only(Estado.fillable);
+
+    // add varios comentarios
+    const estado = await Estado.create(data);
+
+    return estado;
+  }
 }
 
-module.exports = EstadoController
+module.exports = EstadoController;
