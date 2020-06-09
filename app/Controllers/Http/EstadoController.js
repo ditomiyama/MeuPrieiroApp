64402f2087690e26a7ef1a82c32d330b6ba62c5f
ditomiyama <danieltenis@msn.com>
2020-06-09 14:48:17 -0300

    comitando branch Um

--- app/Controllers/Http/EstadoController.js
+++ app/Controllers/Http/EstadoController.js
@@ -13,26 +13,46 @@ class EstadoController {
   async index() {
     const estados = await Estado.query().with("pais").fetch();
     // add varios comentarios
-
+    //retorno
     return estados;
   }
 
-  async show({ params }) {
-    const estado = await Estado.findOrFail(params.id);
-    await estado.load("pais");
-    // add varios comentarios
-    //await estado.getRelated('pais')
-    return estado;
-  }
+  async show ({ params }) {
+    const estado = await Estado.findOrFail(params.id)
+      await estado.load('pais');
+      // add varios comentarios
+      //await estado.getRelated('pais')
+      //teste
+    return estado
+}
 
-  async store({ request, response }) {
+/*async store ({ request, response }){
     const data = request.only(Estado.fillable);
 
     // add varios comentarios
     const estado = await Estado.create(data);
 
     return estado;
+  }*/
+
+
+  async store ({ request, response }){
+    const data = request.only(Estado.fillable);
+    // add varios comentarios
+    //contante
+    const estado = await Estado.create(data);
+    
+    return estado;
   }
+
+  /*async store ({ request, response }){
+    const data = request.only(Estado.fillable);
+
+    const estado = await Estado.create(data);
+
+    return estado;
+  }*/
+
 }
 
 module.exports = EstadoController;
